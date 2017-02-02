d8ed46e6c4b6a56f4487f44b6109e08439eab17f
Admin-PC\Admin <se8endipity@gmail.com>
2017-02-02 13:58:57 +0900

    최신ㅇㅇ

--- JM1/src/Main.java
+++ JM1/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		System.out.println("김슬기");
 		System.out.println("진무");
 		System.out.println("sdsd");
-=======
+
 
 		System.out.println("고흥수");
 		System.out.println("정지호");
